410375e6e39ed99e4a0ce2cef928bdf80f837094
Mohamzeem <Mohamzeem@gmail.com>
2023-01-04 17:07:12 +0200

    second commit

--- lib/features/posts/presentation/pages/posts_page.dart
+++ lib/features/posts/presentation/pages/posts_page.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:posts/core/consts/app_colors.dart';
 
 class PostsScreen extends StatelessWidget {
   const PostsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        extendBody: true,
-        appBar: AppBar(
-          title: const Text('Posts'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Posts',
+          style: TextStyle(
+              color: AppColors.kPrimary,
+              fontSize: 25,
+              fontWeight: FontWeight.w600),
         ),
-        body: Center(
-          // ignore: avoid_unnecessary_containers
-          child: Container(
-            child: const Text('Hello World'),
-          ),
-        ),
+      ),
+      body: Stack(
+        children: [],
       ),
     );
   }
